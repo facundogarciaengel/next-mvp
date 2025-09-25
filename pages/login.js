1f43f4e97cc028ef333bed6f4e9b2f06c0fdf5ab
Facundo Engel <fgarciaengel@gmail.com>
2025-09-25 20:20:30 -0300

    agregamos placeholders descriptivos

--- pages/login.js
+++ pages/login.js
@@ -24,7 +24,7 @@ export default function Login() {
               value={user}
               onChange={(e) => setUser(e.target.value)}
               className="border rounded-lg px-3 py-2 outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="tu_usuario"
+              placeholder="Ingresa cualquier usuario"
               autoComplete="username"
               required
             />
@@ -38,7 +38,7 @@ export default function Login() {
               value={pass}
               onChange={(e) => setPass(e.target.value)}
               className="border rounded-lg px-3 py-2 outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="••••••••"
+              placeholder="Ingrese cualquier contraseña"
               autoComplete="current-password"
               required
             />
